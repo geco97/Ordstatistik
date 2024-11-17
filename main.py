2741943d54cc5f459eb52e9e9f526cfde4285b1e
JACK <geco97@gmail.com>
2024-11-18 00:49:39 +0100

    update

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 #read_file
 def read_file(inputfile):
     try:
@@ -13,21 +14,25 @@ def read_file(inputfile):
 #word_list_count
 def word_list_count(inputText):
     wordList = []
-    words = inputText.strip().split(" ")
-    #print(words)
+    lines = inputText.strip().split('\n')
+    words = []
+    for line in lines:
+        words.extend(line.split(" "))
+    print(words)
     for word in words:
         #print(word)
-        if len(wordList) == 0:
-            wordCount = words.count(word)
-            wordList.append(
-                {"Word":word,"Antal":wordCount}
-            )
-        else:
-            if not any(entry['Word'] == word for entry in wordList):
+        if word != "":
+            if len(wordList) == 0:
                 wordCount = words.count(word)
                 wordList.append(
                     {"Word":word,"Antal":wordCount}
                 )
+            else:
+                if not any(entry['Word'] == word for entry in wordList):
+                    wordCount = words.count(word)
+                    wordList.append(
+                        {"Word":word,"Antal":wordCount}
+                    )
     sortedwordList = sorted(wordList, key= lambda x:x["Word"])
     #print(sortedwordList)
     #print(wordList)
@@ -38,6 +43,19 @@ def readFile(fileName):
         inputText=""
         with open(fileName,"r", encoding='utf-8') as file:
             for line in file:
+                inputText+=line
+            print(inputText)
+    except FileExistsError:
+        print(f"Error: {fileName} finns inte!")
+        return ""
+#readResultFile
+def readResultFile(fileName):
+    try:
+        inputText=""
+        with open(fileName,"r", encoding='utf-8') as file:
+            lines = file.readlines()
+            maxLines = lines[:16]
+            for line in maxLines:
                 inputText+=line
             print(inputText)
     except FileExistsError:
@@ -56,8 +74,11 @@ def save_the_results(wordList,outputFile):
 
 def save_the_most_frequent(wordList,inputFile):
     wordlist_sort = sorted(wordList, key= lambda x:x['Antal'], reverse=True)
+    fileExist = os.path.exists("most_frequent.txt")
     with open("most_frequent.txt","a", encoding='utf-8') as file:
-        file.write('Följande rad bör vara sista raden i most_frequent.txt:\n')
+        if not fileExist:
+            file.write('Följande rad bör vara sista raden i most_frequent.txt:\n')
+
         file_name = inputFile.split('/')[-1]
         file.write(f"{file_name}: {wordlist_sort[0]['Word']} - {wordlist_sort[0]['Antal']}\n")
     readFile('most_frequent.txt')
@@ -71,7 +92,7 @@ def main():
             inputText = read_file(inputFile)    
             wordList = word_list_count(inputText)
             save_the_results(wordList,outputFile)
-            readFile(outputFile)
+            readResultFile(outputFile)
             save_the_most_frequent(wordList,inputFile)
             
         except Exception:
